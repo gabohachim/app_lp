d73c5da48969b05bf541a551054844d2bd48bb55
gabohachim <gabo.hachim@gmail.com>
2025-12-19 17:31:00 +0200

    Update view_mode_service.dart

--- lib/services/view_mode_service.dart
+++ lib/services/view_mode_service.dart
@@ -1,1 +1,16 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ViewModeService {
+  static const _kGrid = 'vinyl_view_grid';
+
+  static Future<bool> isGridEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_kGrid) ?? false; // default: lista
+  }
+
+  static Future<void> setGridEnabled(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_kGrid, value);
+  }
+}
 
